90ac219d7316af5a3662238fabb561cb8406246d
stolbivi <viktor.stolbin@gmail.com>
2021-04-09 14:55:10 -0400

    Upload script

---
+++ scripts/DynamoDBData.js
@@ -0,0 +1,38 @@
+module.exports = {
+  itemToJSON: (item) => {
+    let result = {};
+    for (let key in item) {
+      if (Object.prototype.hasOwnProperty.call(item, key)) {
+        if (item[key].S || item[key].S === "") {
+          // @ts-ignore
+          result[key] = item[key].S;
+        } else if (item[key].N) {
+          // @ts-ignore
+          result[key] = Number(item[key].N);
+        } else if (item[key].BOOL === false || item[key].BOOL == true) {
+          // @ts-ignore
+          result[key] = Boolean(item[key].BOOL);
+        }
+      }
+    }
+    return result;
+  },
+  itemFromJSON: (json) => {
+    let result = {};
+    for (let key in json) {
+      if (Object.prototype.hasOwnProperty.call(json, key)) {
+        if (typeof json[key] === "string") {
+          // @ts-ignore
+          result[key] = { S: json[key].toString() };
+        } else if (typeof json[key] === "number") {
+          // @ts-ignore
+          result[key] = { N: json[key].toString() };
+        } else if (typeof json[key] === "boolean") {
+          // @ts-ignore
+          result[key] = { BOOL: json[key].toString() };
+        }
+      }
+    }
+    return result;
+  },
+};
---
+++ scripts/upload.js
@@ -0,0 +1,72 @@
+require("dotenv").config();
+// const sleep = require("sleep");
+const args = require("minimist")(process.argv.slice(2));
+const csv = require("csv-parser");
+const fs = require("fs");
+const {
+  DynamoDBClient,
+  BatchWriteItemCommand,
+} = require("@aws-sdk/client-dynamodb");
+const { itemFromJSON } = require("./DynamoDBData");
+
+const dynamoDbClient = new DynamoDBClient({ region: process.env.AWS_REGION });
+
+let fileName = args["file"];
+let source = args["source"];
+let id = Number(args["id"]);
+console.log("Reading file:", fileName);
+
+function getTimestamp(dateTime) {
+  return Math.floor(new Date(dateTime).getTime() / 1000);
+}
+
+function getItem(id, source, row) {
+  return {
+    id,
+    source,
+    pickupTime: getTimestamp(row.pickup_datetime),
+    dropOffTime: getTimestamp(row.dropoff_datetime),
+    passengerCount: Number(row.passenger_count),
+    distance: Number(row.trip_distance),
+    startId: Number(row.PULocationID),
+    endId: Number(row.DOLocationID),
+    fare: Number(row.fare_amount),
+    extra: Number(row.extra),
+    mtaTax: Number(row.mta_tax),
+    tip: Number(row.tip_amount),
+    tolls: Number(row.tolls_amount),
+    improvementSurcharge: Number(row.improvement_surcharge),
+    congestionSurcharge: Number(row.congestion_surcharge),
+    total: Number(row.total_amount),
+  };
+}
+
+let data = [];
+fs.createReadStream(fileName)
+  .pipe(csv())
+  .on("data", (row) => {
+    data.push(getItem(id, source, row));
+    id++;
+  })
+  .on("end", async () => {
+    console.log(
+      `File ${fileName} successfully processed with ${data.length} records`
+    );
+
+    let batch = [];
+    for (let i = 0; i < data.length; i++) {
+      let item = data[i];
+      console.log(item.id);
+      if (batch.length >= process.env.BATCH_SIZE) {
+        const command = new BatchWriteItemCommand({
+          RequestItems: { [process.env.TABLE_TLC]: batch },
+        });
+        await dynamoDbClient.send(command);
+        // sleep.msleep(10);
+        batch.length = 0;
+      } else {
+        batch.push({ PutRequest: { Item: itemFromJSON(item) } });
+      }
+    }
+    console.log(`Last index ${id}`);
+  });
